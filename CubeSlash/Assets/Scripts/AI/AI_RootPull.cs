69f79e22b476562e488d7b89cab9c270d5e83f5d
flawlesshappiness <bkk@arabesque.dk>
2023-06-11 23:17:59 +0200

    Added RootPull enemy

---
+++ CubeSlash/Assets/Scripts/AI/AI_RootPull.cs
@@ -0,0 +1,51 @@
+using Flawliz.Lerp;
+using UnityEngine;
+
+public class AI_RootPull : EnemyAI
+{
+    [SerializeField] private RootPullVine _vine;
+
+    public float max_dist_to_player;
+
+    private Vector3 pos_player_prev;
+
+    private bool _attached;
+
+    public override void Initialize(Enemy enemy)
+    {
+        base.Initialize(enemy);
+    }
+
+    private void FixedUpdate()
+    {
+        pos_player_prev = IsPlayerAlive() ? Player.Instance.transform.position : pos_player_prev;
+        MoveUpdate();
+    }
+
+    private void MoveUpdate()
+    {
+        var dist = DistanceToPlayer();
+        var dist_max = CameraController.Instance.Width * max_dist_to_player;
+        if (dist < dist_max)
+        {
+            MoveToStop(0.5f);
+            TurnTowards(pos_player_prev);
+            AttachToPlayer();
+        }
+        else
+        {
+            MoveTowards(pos_player_prev);
+            TurnTowards(pos_player_prev);
+        }
+    }
+
+    private void AttachToPlayer()
+    {
+        if (_attached) return;
+        if (Player.Instance == null) return;
+        if (Player.Instance.IsDead) return;
+        _attached = true;
+        _vine.target = Player.Instance.Rigidbody;
+        _vine.AnimateToTarget();
+    }
+}
---
+++ CubeSlash/Assets/Scripts/Enemy/RootPullVine.cs
@@ -0,0 +1,118 @@
+using Flawliz.Lerp;
+using System.Collections;
+using UnityEngine;
+
+public class RootPullVine : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer spr_main;
+    [SerializeField] private Transform pivot_main;
+    [SerializeField] private ParticleSystem ps_pull;
+
+    public float pull_force;
+    public float drawmode_size = 0.32f;
+    public float drawmode_position = 0.16f;
+    [Min(0)] public float scale;
+
+
+    public Rigidbody2D target;
+
+    private float SpriteScale { get { return spr_main.transform.lossyScale.y; } }
+    private float UnitToScale { get { return 1f / (drawmode_position * SpriteScale * 2); } }
+    private float DistanceToTarget { get { return Vector3.Distance(transform.position, target.transform.position); } }
+
+    private bool animating;
+
+    private void OnValidate()
+    {
+        if (Application.isPlaying) return;
+        if (target != null) UpdateScale();
+        UpdateVisual();
+    }
+
+    private void Update()
+    {
+        if (animating) return;
+        UpdateScale();
+        UpdateVisual();
+    }
+
+    private void UpdateVisual()
+    {
+        UpdateSize();
+        UpdateRotation();
+        UpdateForce();
+        UpdateParticles();
+    }
+
+    private void UpdateScale()
+    {
+        scale = GetScaleToTarget();
+    }
+
+    private float GetScaleToTarget() => target == null ? 0 : DistanceToTarget * UnitToScale;
+
+    private void UpdateSize()
+    {
+        if (spr_main == null) return;
+        spr_main.size = new Vector2(drawmode_size, drawmode_size * scale);
+        spr_main.transform.localPosition = new Vector3(0, drawmode_position * spr_main.transform.localScale.y * scale);
+    }
+
+    private void UpdateRotation()
+    {
+        if (pivot_main == null) return;
+        if (target == null) return;
+        pivot_main.rotation = transform.RotationTo(target.transform);
+    }
+
+    private void UpdateForce()
+    {
+        if(target == null) return;
+        var dir = transform.DirectionTo(target.transform);
+        target.AddForce(-dir * pull_force);
+    }
+
+    private void UpdateParticles()
+    {
+        if(target == null)
+        {
+            ps_pull.SetEmissionEnabled(false);
+            return;
+        }
+
+        if (ps_pull == null) return;
+
+        ps_pull.SetEmissionEnabled(true);
+
+        var dist = DistanceToTarget;
+        ps_pull.ModifyShape(m =>
+        {
+            m.scale = m.scale.SetZ(dist);
+            m.position = m.position.SetY(dist * 0.5f);
+        });
+        ps_pull.ModifyEmission(m =>
+        {
+            m.rateOverTime = dist * 2;
+        });
+    }
+
+    public Coroutine AnimateToTarget()
+    {
+        if (target == null) return null;
+
+        return StartCoroutine(Cr());
+
+        IEnumerator Cr()
+        {
+            animating = true;
+            var curve = EasingCurves.EaseOutQuad;
+            yield return LerpEnumerator.Value(0.5f, f =>
+            {
+                var t = curve.Evaluate(f);
+                scale = Mathf.Lerp(0f, GetScaleToTarget(), t);
+                UpdateVisual();
+            });
+            animating = false;
+        }
+    }
+}
