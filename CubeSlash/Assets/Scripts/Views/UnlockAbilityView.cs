b610e8fdcd7aa02d417d0a93e448c2cb8460b7fd
flawlesshappiness <bkk@arabesque.dk>
2023-07-19 23:22:46 +0200

    Changed Unlock views

--- CubeSlash/Assets/Scripts/Views/UnlockAbilityView.cs
+++ CubeSlash/Assets/Scripts/Views/UnlockAbilityView.cs
@@ -1,18 +1,11 @@
 using Flawliz.Lerp;
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
-using UnityEngine.EventSystems;
 
 public class UnlockAbilityView : View
 {
-    [SerializeField] private UIIconButton temp_btn_ability;
-    [SerializeField] private TMP_Text tmp_desc;
-    [SerializeField] private RectTransform pivot_ability_bar;
-    [SerializeField] private CanvasGroup cvg_background, cvg_abilities, cvg_description;
-    [SerializeField] private UIUnlockAbilityBar ability_bar;
-
+    [SerializeField] private CanvasGroup cvg_background;
     [SerializeField] private RadialMenu radial;
     [SerializeField] private UIFloatingPanel floating_panel;
     [SerializeField] private UIFloatingPanelUpgrade floating_upgrade;
@@ -20,44 +13,21 @@ public class UnlockAbilityView : View
     public event System.Action OnAbilitySelected;
     public event System.Action OnSkip;
 
-    private List<UIIconButton> btns_ability = new List<UIIconButton>();
+    private void Awake()
+    {
+        cvg_background.alpha = 0;
+    }
 
     private void Start()
     {
-        temp_btn_ability.gameObject.SetActive(false);
-
         var is_first = AbilityController.Instance.GetGainedAbilities().Count == 0;
         var abilities = AbilityController.Instance.GetAvailableAbilities().TakeRandom(2);
 
+        HideFloatingPanel();
+        radial.OnSelect += OnSelect;
         AddRadialMenuElements(abilities);
 
-        HideFloatingPanel();
-
-        radial.OnSelect += OnSelect;
-
-        /*
-        DisplayAbility(abilities[0]);
-        ClearButtons();
-        foreach (var ability in abilities)
-        {
-            var btn = CreateButton();
-            btn.Icon = ability.Info.sprite_icon;
-            btn.Button.onSelect += () => DisplayAbility(ability);
-            btn.Button.onSubmit += () => Click(btn, ability);
-        }
-
-        // Animate
-        StartCoroutine(AnimateStartCr());
-
-        void Click(UIIconButton btn, Ability ability)
-        {
-            AbilityController.Instance.GainAbility(ability.Info.type);
-            AbilityController.Instance.AddModifier(ability.Info.type);
-            SoundController.Instance.Play(SoundEffectType.sfx_ui_unlock_ability);
-            OnAbilitySelected?.Invoke();
-            Close(0);
-        }
-        */
+        Lerp.Alpha(cvg_background, 0.5f, 0.5f).UnscaledTime();
     }
 
     private void OnSelect(RadialMenuElement element)
@@ -92,6 +62,7 @@ public class UnlockAbilityView : View
             {
                 Sprite = ability.Info.sprite_icon,
                 OnSelect = () => UpdateDisplayText(ability),
+                OnSubmitBegin = OnSubmitBegin,
                 OnSubmitComplete = () => SelectAbility(ability)
             });
         }
@@ -108,12 +79,19 @@ public class UnlockAbilityView : View
         options.Insert(0, new RadialMenuOption
         {
             Sprite = Icon.Get(IconType.arrow_back),
+            OnSelect = HideFloatingPanel,
             OnSubmitComplete = Skip
         });
 
         radial.AddOptions(options);
         radial.AnimateShowElements(true, 0.05f);
         radial.SetCancelElement(radial.GetElement(0));
+    }
+
+    private void OnSubmitBegin()
+    {
+        Lerp.Alpha(cvg_background, 0.5f, 0).UnscaledTime();
+        HideFloatingPanel();
     }
 
     private void SelectAbility(Ability ability)
@@ -152,89 +130,5 @@ public class UnlockAbilityView : View
     {
         OnSkip?.Invoke();
         Close(0);
-    }
-
-    private UIIconButton CreateButton()
-    {
-        var btn = Instantiate(temp_btn_ability, temp_btn_ability.transform.parent);
-        btn.gameObject.SetActive(true);
-        btns_ability.Add(btn);
-        return btn;
-    }
-
-    private void ClearButtons()
-    {
-        btns_ability.ForEach(b => Destroy(b.gameObject));
-        btns_ability.Clear();
-    }
-
-    private void DisplayAbility(Ability ability)
-    {
-        var text = "";
-
-        if (ability != null)
-        {
-            //text += ability.Info.name_ability;
-            //text += "\n";
-            text += ability.Info.desc_ability;
-        }
-
-        tmp_desc.text = text;
-    }
-
-    IEnumerator AnimateStartCr()
-    {
-        Interactable = false;
-        ability_bar.CanvasGroup.alpha = 0;
-        cvg_background.alpha = 0;
-        cvg_abilities.alpha = 0;
-        cvg_description.alpha = 0;
-
-        Lerp.Alpha(ability_bar.CanvasGroup, 0.25f, 1).UnscaledTime();
-        Lerp.Alpha(cvg_background, 0.25f, 1).UnscaledTime();
-
-        ability_bar.SetPreviousValue();
-        yield return new WaitForSecondsRealtime(0.25f);
-        yield return ability_bar.AnimateLevelsUntilAbility(1.5f, EasingCurves.EaseOutQuad);
-        yield return LerpEnumerator.AnchoredPosition(pivot_ability_bar, 0.5f, pivot_ability_bar.anchoredPosition.AddY(300))
-            .Curve(EasingCurves.EaseOutQuad)
-            .UnscaledTime();
-
-        cvg_abilities.alpha = 1;
-        yield return AnimateButtons();
-
-        cvg_description.alpha = 1;
-        Interactable = true;
-
-        // Set selection
-        EventSystem.current.SetSelectedGameObject(btns_ability[0].gameObject);
-    }
-
-    private Coroutine AnimateButtons()
-    {
-        return StartCoroutine(AnimateButtonsCr());
-        IEnumerator AnimateButtonsCr()
-        {
-            var crs = new List<Lerp>();
-            btns_ability.ForEach(btn => btn.AnimationPivot.localScale = Vector3.zero);
-            foreach (var btn in btns_ability)
-            {
-                var pivot = btn.AnimationPivot;
-                pivot.transform.position = btn.transform.position;
-                var lerp = Lerp.LocalScale(pivot, 0.25f, Vector3.zero, Vector3.one).Curve(EasingCurves.EaseOutQuad).UnscaledTime();
-                crs.Add(lerp);
-                yield return new WaitForSecondsRealtime(0.1f);
-            }
-
-            yield return StartCoroutine(WaitForCoroutinesCr(crs));
-        }
-
-        IEnumerator WaitForCoroutinesCr(List<Lerp> crs)
-        {
-            foreach (var cr in crs)
-            {
-                yield return cr;
-            }
-        }
     }
 }
--- CubeSlash/Assets/Scripts/Views/UnlockUpgradeView.cs
+++ CubeSlash/Assets/Scripts/Views/UnlockUpgradeView.cs
@@ -1,95 +1,31 @@
 using Flawliz.Lerp;
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
-using UnityEngine.InputSystem;
-using UnityEngine.UI;
 
 public class UnlockUpgradeView : View
 {
-    [SerializeField] private UIUnlockedUpgradesLayout layout_unlocked_upgrades;
-    [SerializeField] private UIIconButton temp_btn_upgrade;
-    [SerializeField] private TMP_Text tmp_upgrade;
-    [SerializeField] private RectTransform rt_upgrades, rt_desc;
-    [SerializeField] private UIInputLayout input_layout_main, input_layout_refund;
-    [SerializeField] private UIUnlockAbilityBar ability_bar;
-    [SerializeField] private Image img_fg_refund;
-    [SerializeField] private CanvasGroup cvg_upgrades, cvg_background, cvg_description, cvg_past_upgrades;
-    [SerializeField] private UIScrollableUpgradeTree template_upgrade_tree;
+    [SerializeField] private CanvasGroup cvg_background;
+    [SerializeField] private RadialMenu radial;
     [SerializeField] private UIFloatingPanel floating_panel;
-    [SerializeField] private UIFloatingPanelUpgrade floating_panel_upgrade;
+    [SerializeField] private UIFloatingPanelUpgrade floating_upgrade;
 
     public event System.Action<Upgrade> OnUpgradeSelected;
 
-    private List<UIIconButton> btns_upgrade = new List<UIIconButton>();
-    private List<TMP_Text> tmps_upgrade = new List<TMP_Text>();
-    private List<UIScrollableUpgradeTree> trees = new List<UIScrollableUpgradeTree>();
-
-    private UIScrollableUpgradeTree selected_tree;
-
-    private Coroutine cr_refund;
-
-    private FMODEventInstance sfx_refund_hold;
+    private void Awake()
+    {
+        cvg_background.alpha = 0;
+    }
 
     private void Start()
     {
-        // Initialize
-        temp_btn_upgrade.gameObject.SetActive(false);
-        tmp_upgrade.gameObject.SetActive(false);
-        template_upgrade_tree.gameObject.SetActive(false);
+        var upgrades = GetRandomUpgrades();
 
-        layout_unlocked_upgrades.OnUpgradeLevelSelected += (btn, upgrade) => DisplayUpgradeText(btn, upgrade);
+        HideFloatingPanel();
+        radial.OnSelect += OnSelect;
+        AddRadialMenuElements(upgrades);
 
-        // Upgrades
-        CreateUpgradeTrees();
-
-        // Input
-        DisplayInput();
-
-        StartCoroutine(AnimateStartCr());
-    }
-
-    private void OnEnable()
-    {
-        PlayerInput.Controls.Player.West.started += RefundPressed;
-        PlayerInput.Controls.Player.West.canceled += RefundReleased;
-    }
-
-    private void OnDisable()
-    {
-        PlayerInput.Controls.Player.West.started -= RefundPressed;
-        PlayerInput.Controls.Player.West.canceled -= RefundReleased;
-        sfx_refund_hold?.Stop();
-    }
-
-    private IEnumerator AnimateStartCr()
-    {
-        cvg_background.alpha = 0;
-        cvg_description.alpha = 0;
-        cvg_past_upgrades.alpha = 0;
-        input_layout_main.CanvasGroup.alpha = 0;
-        input_layout_refund.CanvasGroup.alpha = 0;
-        ability_bar.CanvasGroup.alpha = 0;
-        cvg_upgrades.alpha = 1;
-        Interactable = false;
-
-        yield return AnimateShowUpgradeTree();
-
-        // Show other elements
-        Interactable = true;
-        var first_tree = trees[0];
-        first_tree.SetChildUpgradesVisible(true);
-        first_tree.Select();
-        DisplayUpgradeText(first_tree.MainButton, first_tree.MainInfo.upgrade);
-
-        cvg_description.alpha = 1;
-        cvg_past_upgrades.alpha = 1;
-        input_layout_main.CanvasGroup.alpha = 1;
-        input_layout_refund.CanvasGroup.alpha = 1;
-        ability_bar.CanvasGroup.alpha = 1;
-
-        ability_bar.AnimateLevelsUntilAbility(1f, EasingCurves.EaseOutQuad);
+        Lerp.Alpha(cvg_background, 0.5f, 0.5f).UnscaledTime();
     }
 
     private List<UpgradeInfo> GetRandomUpgrades()
@@ -100,173 +36,81 @@ public class UnlockUpgradeView : View
         return upgrades;
     }
 
-    private void ClickUpgradeButton(Upgrade upgrade)
+    private void OnSelect(RadialMenuElement element)
     {
-        if (!Interactable) return;
-        Interactable = false;
-        CanvasGroup.interactable = false;
-        CanvasGroup.blocksRaycasts = false;
-        UpgradeController.Instance.UnlockUpgrade(upgrade.id);
+        if (element == null)
+        {
+            HideFloatingPanel();
+        }
+        else
+        {
+            floating_panel.SetTarget(element.transform, new Vector2(50, 50));
+        }
+    }
+
+    private void HideFloatingPanel()
+    {
+        floating_panel.gameObject.SetActive(false);
+    }
+
+    private void AddRadialMenuElements(List<UpgradeInfo> upgrades)
+    {
+        radial.Clear();
+
+        var ability_icon = AbilityController.Instance.GetAbilityPrefab(Save.PlayerBody.primary_ability).Info.sprite_icon;
+        var difficulty_icon = DifficultyController.Instance.Difficulty.difficulty_sprite;
+
+        var options = new List<RadialMenuOption>();
+
+        foreach (var info in upgrades)
+        {
+            options.Add(new RadialMenuOption
+            {
+                Sprite = info.upgrade.icon,
+                OnSelect = () => UpdateDisplayText(info),
+                OnSubmitBegin = OnSubmitBegin,
+                OnSubmitComplete = () => SelectUpgrade(info)
+            });
+        }
+
+        radial.AddOptions(options);
+        radial.AnimateShowElements(true, 0.05f);
+        radial.SetCancelElement(radial.GetElement(0));
+    }
+
+    private void SelectUpgrade(UpgradeInfo info)
+    {
+        UpgradeController.Instance.UnlockUpgrade(info.upgrade.id);
         SoundController.Instance.Play(SoundEffectType.sfx_ui_unlock_upgrade);
+        OnUpgradeSelected?.Invoke(info.upgrade);
+        Close(0);
+    }
+
+    private void UpdateDisplayText(UpgradeInfo info)
+    {
+        floating_panel.gameObject.SetActive(true);
+        floating_upgrade.Clear();
+
+        var modifiers = info.upgrade.modifiers;
+        foreach (var modifier in modifiers)
+        {
+            var attribute = GameAttributeController.Instance.GetAttribute(modifier.attribute_type);
+            floating_upgrade.AddModifiedAttribute(attribute, modifier);
+        }
 
         StartCoroutine(Cr());
         IEnumerator Cr()
         {
-            CanvasGroup.alpha = 0;
-            yield return new WaitForSecondsRealtime(0.25f);
-            GameController.Instance.LerpTimeScale(0.5f, 1);
-            OnUpgradeSelected?.Invoke(upgrade);
-            yield return new WaitForSecondsRealtime(0.5f);
-            Close(0);
+            floating_panel.CanvasGroup.alpha = 0;
+            yield return null;
+            floating_panel.ContentSizeFitterRefresh.RefreshContentFitters();
+            floating_panel.CanvasGroup.alpha = 1;
         }
     }
 
-    private void CreateUpgradeTrees()
+    private void OnSubmitBegin()
     {
-        var upgrades = GetRandomUpgrades();
-        foreach (var info in upgrades)
-        {
-            var tree = Instantiate(template_upgrade_tree, template_upgrade_tree.transform.parent);
-            tree.gameObject.SetActive(true);
-            tree.Initialize(info);
-            tree.SetChildUpgradesVisible(false, 0);
-            trees.Add(tree);
-
-            tree.onMainButtonSelected += btn => OnMainButtonSelected(tree);
-            tree.onUpgradeSelected += (btn, u) => DisplayUpgradeText(btn, u.upgrade);
-            tree.MainButton.Button.onSubmit += () => ClickUpgradeButton(info.upgrade);
-        }
-
-        void OnMainButtonSelected(UIScrollableUpgradeTree tree)
-        {
-            if (selected_tree != null)
-            {
-                selected_tree.ScrollToPosition(selected_tree.MainButton.transform.position);
-                selected_tree.SetChildUpgradesVisible(false);
-            }
-
-            selected_tree = tree;
-
-            if (selected_tree != null)
-            {
-                selected_tree.SetChildUpgradesVisible(true);
-            }
-        }
-    }
-
-    private Coroutine AnimateShowUpgradeTree()
-    {
-        return StartCoroutine(Cr());
-        IEnumerator Cr()
-        {
-            Lerp.Alpha(cvg_background, 0.5f, 1).UnscaledTime();
-
-            foreach (var tree in trees)
-            {
-                tree.AnimateShowMainButton();
-                yield return new WaitForSecondsRealtime(0.1f);
-            }
-
-            foreach (var tree in trees)
-            {
-                tree.AnimateShowParentButtons();
-                tree.AnimateShowChildButtons();
-                yield return new WaitForSecondsRealtime(0.1f);
-            }
-        }
-    }
-
-    private TMP_Text CreateUpgradeText(string text)
-    {
-        var tmp = Instantiate(tmp_upgrade, tmp_upgrade.transform.parent);
-        tmp.gameObject.SetActive(true);
-        tmp.text = text;
-        tmps_upgrade.Add(tmp);
-        return tmp;
-    }
-
-    private void ClearUpgradeTexts()
-    {
-        tmps_upgrade.ForEach(t => Destroy(t.gameObject));
-        tmps_upgrade.Clear();
-    }
-
-    private void DisplayUpgradeText(UIIconButton btn, Upgrade upgrade)
-    {
-        floating_panel_upgrade.Clear();
-        var modifiers = upgrade.modifiers;
-        foreach (var modifier in modifiers)
-        {
-            var attribute = GameAttributeController.Instance.GetAttribute(modifier.attribute_type);
-            floating_panel_upgrade.AddModifiedAttribute(attribute, modifier);
-        }
-
-        floating_panel.SetTarget(btn.transform, new Vector2(50, 50));
-        floating_panel.ContentSizeFitterRefresh.RefreshContentFitters();
-
-        ClearUpgradeTexts();
-    }
-
-    private void DisplayInput()
-    {
-        input_layout_main.Clear();
-        input_layout_main.AddInput(PlayerInput.UIButtonType.SOUTH, "Select");
-        input_layout_refund.Clear();
-        input_layout_refund.AddInput(PlayerInput.UIButtonType.WEST, "(HOLD)");
-    }
-
-    private void RefundPressed(InputAction.CallbackContext context)
-    {
-        if (!Interactable) return;
-        Interactable = false;
-        HoldRefund(true);
-    }
-
-    private void RefundReleased(InputAction.CallbackContext context)
-    {
-        Interactable = true;
-        HoldRefund(false);
-    }
-
-    private void HoldRefund(bool holding)
-    {
-        if (holding)
-        {
-            if (cr_refund != null) return;
-            cr_refund = StartCoroutine(RefundCr());
-        }
-        else
-        {
-            if (cr_refund == null) return;
-            StopCoroutine(cr_refund);
-            cr_refund = null;
-            img_fg_refund.SetAlpha(0);
-            sfx_refund_hold?.Stop();
-        }
-
-        IEnumerator RefundCr()
-        {
-            sfx_refund_hold = SoundController.Instance.Play(SoundEffectType.sfx_ui_refund_hold);
-
-            var anim = LerpEnumerator.Alpha(img_fg_refund, 2f, 0f, 0.25f);
-            anim.AnimationCurve = EasingCurves.EaseInQuad;
-            anim.UseUnscaledTime = true;
-            yield return anim;
-
-            sfx_refund_hold?.Stop();
-
-            Refund();
-        }
-    }
-
-    private void Refund()
-    {
-        SoundController.Instance.Play(SoundEffectType.sfx_ui_refund);
-
-        Player.Instance.Experience.Value += Player.Instance.Experience.Max * 0.25f;
-
-        var view = ViewController.Instance.ShowView<AbilityView>(0, GameController.TAG_ABILITY_VIEW);
-        view.OnContinue += GameController.Instance.ResumeLevel;
-        Close(0);
+        Lerp.Alpha(cvg_background, 0.5f, 0).UnscaledTime();
+        HideFloatingPanel();
     }
 }
