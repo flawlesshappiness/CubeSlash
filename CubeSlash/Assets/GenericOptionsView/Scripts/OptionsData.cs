08aa17d602ece5b3dd3c1d129046a0815a34240e
flawlesshappiness <bkk@arabesque.dk>
2023-09-03 15:14:08 +0200

    Fixed initial resolution

--- CubeSlash/Assets/GenericOptionsView/Scripts/OptionsData.cs
+++ CubeSlash/Assets/GenericOptionsView/Scripts/OptionsData.cs
@@ -7,7 +7,7 @@ namespace Flawliz.GenericOptions
         public float MasterVolume { get; set; } = 0.5f;
         public float MusicVolume { get; set; } = 1.0f;
         public float SFXVolume { get; set; } = 1.0f;
-        public Resolution Resolution { get; set; }
+        public Resolution Resolution { get; set; } = new Resolution { width = 1920, height = 1080, refreshRate = 60 };
         public FullScreenMode FullScreenMode { get; set; } = FullScreenMode.FullScreenWindow;
         public bool Vsync { get; set; } = true;
         public int FrameRateCap { get; set; } = 60;
